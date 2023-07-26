8311fff2aeac347c919ff7147a9645f5f586be87
“”fawadahmadwork <fawadahmad99h@gmail.comssh-keygen -t rsa -C fawadahmad99h@gmail.com>
2023-07-26 16:11:32 +0500

    initial commit add devise and active admin

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users, confirmable: true
+  get 'index', to: 'home#index'
+end
