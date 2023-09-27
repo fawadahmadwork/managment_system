56ca7cff31175bca1ed25201fd92418be78f7b5e
Fawad Ahmad <fawadahmad@example.com>
2023-09-27 16:56:22 +0500

    litle change in work aniversery mail schudule

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ set :environment, 'development'
 every '1 0 1 * *' do
   rake 'jobs:send_birthday_emails_to_admins'
 end
-every 1.day, at: '12:01 am' do
+every '1 0 1,11,21 * *' do
   rake 'jobs:send_work_anniversary_emails_to_admins'
 end
