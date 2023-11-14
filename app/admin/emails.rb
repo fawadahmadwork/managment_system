37347d5556ef2eb0bbd8a5d94651ebbac9cd66bc
Fawad Ahmad <fawadahmad@example.com>
2023-11-14 16:44:56 +0500

    add email to active admin

---
+++ app/admin/emails.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Email do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+   permit_params :email, :employee_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :employee_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
