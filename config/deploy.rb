944b0ad8fe48babc2ddb253725d844aec1463842
Fawad Ahmad <fawadahmad@example.com>
2023-11-15 20:34:18 +0500

    deleteing phone_numbers from active admin

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 ## Defaults:
 # set :scm,           :git
 #  set :branch,        :main
- set :bundle_jobs, 8 # Set the number of parallel jobs
+
+#  set :bundle_jobs, 8 # Set the number of parallel jobs
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
