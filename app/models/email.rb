2d7e7123f68e1e8472e8690253fd61199dca6370
Fawad Ahmad <fawadahmad@example.com>
2023-11-15 16:41:50 +0500

    deleting repeat validations

--- app/models/email.rb
+++ app/models/email.rb
@@ -2,6 +2,5 @@ class Email < ApplicationRecord
   belongs_to :employee
 
   validates :email, presence: true, uniqueness: true,
-                    format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' },
-                    uniqueness: true
+                    format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must be a valid email address' }
 end
--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -2,6 +2,5 @@ class PhoneNumber < ApplicationRecord
   belongs_to :employee
 
   validates :phone_number, presence: true, uniqueness: true,
-                           format: { with: /\A\d{4}-\d{7}\z/, message: 'must be in the format 0300-1234567' },
-                           uniqueness: true
+                           format: { with: /\A\d{4}-\d{7}\z/, message: 'must be in the format 0300-1234567' }
 end
